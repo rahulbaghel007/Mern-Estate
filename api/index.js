3352cccef9d154422838fa44ee06ecaf6abbfb6a
rahulbaghel007 <codeforbest@gmail.com>
2023-10-20 19:44:13 +0530

    someother files

--- api/index.js
+++ api/index.js
@@ -1,5 +1,9 @@
 import express from "express";
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
+mongoose.connect(process.env.MONGO);  
 const app = express();
 app.listen(3000,()=>{
     console.log('server is tunning on port 300')
